ea28d141858b72009f27cc8902d5944479de2fd2
Antonio <antoniodelaiglesia@gmail.com>
2023-02-22 11:18:55 +0100

    Feat: First configuration Postgres

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./src/routes/user");
+const sequelize = require("./src/services/db");
 
 const app = express();
 const port = 8000;
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (request: Request, response: Response) => {
   response.json("Hello World!");
 });
+
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
   console.log("fisting on port " + port);
